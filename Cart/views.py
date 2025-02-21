6e53148d46bac14b7dc7f1760b85b05c770b72e4
HaneenHamchou <haneen.hamchou@gmail.com>
2025-02-21 13:24:43 +0100

    Create CartItem  Model

--- Cart/views.py
+++ Cart/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render # type: ignore
-from Cart.models import Product
+from django.shortcuts import redirect, render # type: ignore
+from Cart.models import Product, CartItem
 
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
     response = render(request, "cart/index.html", {'products':products})
     return response
+
+def view_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'cart/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(product=product, 
+                                                       user=request.user)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('cart:view_cart')
