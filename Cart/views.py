8f041cc6af9b6bf2ff25ef2e9f2a09b4664f210b
HaneenHamchou <haneen.hamchou@gmail.com>
2025-02-21 12:42:29 +0100

    Welcome to my E-commerce project

--- Cart/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render # type: ignore
-from Cart.models import Product
-
-# Create your views here.
-def product_list(request):
-    products = Product.objects.all()
-    response = render(request, "cart/index.html", {'products':products})
-    return response
