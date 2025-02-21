e63ce45c6bab3dcae22ebb988abacf6f23143664
HaneenHamchou <haneen.hamchou@gmail.com>
2025-02-21 12:52:54 +0100

    First Product Model

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,6 +20,7 @@ from Cart.views import product_list
 from ecommerce import  settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', product_list)
